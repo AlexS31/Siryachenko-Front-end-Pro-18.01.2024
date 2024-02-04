6177b058ee714f5ea2b3303def0c7c7287bc4b06
Alex <sanya54935@gmail.com>
2024-02-04 17:03:48 +0200

    +

--- Siryachenko-Front-end-Pro-18.01.2024/main.js
+++ Siryachenko-Front-end-Pro-18.01.2024/main.js
@@ -1,15 +1,2 @@
-let getNumberExplanation = prompt ('Введите номер');
-
-switch (Number) {
-case 666:
-alert ('devil number');
-break;
-case 42:
-alert ('answer for everything');
-break;
-case (7):
-alert ('prime number');
-break;
-default:
-alert ('just a number');
-}
+const userName = prompt("Будь ласка, введіть ваше ім'я:");
+alert(`Hello, ${userName}  ! How are you?`);
